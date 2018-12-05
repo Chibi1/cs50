449dc1bb177197bba18a226dc2c5fe6158032085
Chibi1 <chibueze.e.njoku@gmail.com>
2018-12-05 20:27:00 +1100

    small commit

--- finalProject/script.js
+++ finalProject/script.js
@@ -1,1 +1,17 @@
 console.log("Hello World");
+
+$(document).ready(function(){
+    // get digit
+    // var value = document.getElementById("digit").onclick = function() {
+    //     return this.value;
+    // }
+    function getValue(val) {
+        alert(val.value);
+        
+    }
+
+    // cancel out post button click focus
+    $("btn").mouseup(function(){
+        $(this).blur();
+    })
+})
