8055e3d97dc6b348e30f66b3cd1c9ba8e416112d
Chibi1 <chibueze.e.njoku@gmail.com>
2018-12-05 21:09:28 +1100

    button values are now being recorded

--- finalProject/script.js
+++ finalProject/script.js
@@ -5,10 +5,15 @@ $(document).ready(function(){
     // var value = document.getElementById("digit").onclick = function() {
     //     return this.value;
     // }
-    function getValue(val) {
-        alert(val.value);
-        
-    }
+    // function getValue() {
+    //     var digit = document.getElementById("digit").value;
+    //     console.log(this.value);
+    // }
+    // $("#digit").click(getValue);
+    $("input").click(function() {
+        var value = $(this).val();
+        console.log(value);
+    })
 
     // cancel out post button click focus
     $("btn").mouseup(function(){
